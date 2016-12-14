395084df6331fec8908af18e4893e1f2d015146c
lzb <kulou322@126.com>
2016-12-14 16:07:39 +0800

    初始化

---
+++ 从日历选择日期/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  从日历选择日期
+//
+//  Created by lzb on 2016/12/14.
+//  Copyright © 2016年 lzb的工程. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 从日历选择日期/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  从日历选择日期
+//
+//  Created by lzb on 2016/12/14.
+//  Copyright © 2016年 lzb的工程. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
